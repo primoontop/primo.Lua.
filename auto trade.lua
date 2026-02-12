236ce7ee09e679e1e6882e135a41bf648282bf5f
primoontop <primoontop3@gmail.com>
2026-02-12 19:48:18 +0800

    Update auto trade.lua

--- auto trade.lua
+++ auto trade.lua
@@ -1,6 +1,6 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/memejames/elerium-v2-ui-library//main/Library", true))()
 
-local window = library:AddWindow("Primo Public Script", {
+local window = library:AddWindow("AutoTrade Public Script|MADE BY-Primo", {
     main_color = Color3.fromRGB(0, 0, 0),
     min_size = Vector2.new(620, 820),
 })
