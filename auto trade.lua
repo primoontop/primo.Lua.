bfa711c2f580c26be0570edb29da6476d432accc
primoontop <primoontop3@gmail.com>
2026-02-12 19:30:18 +0800

    Update auto trade.lua

--- auto trade.lua
+++ auto trade.lua
@@ -2,7 +2,7 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/memej
 
 local window = library:AddWindow("Primo Public Script", {
     main_color = Color3.fromRGB(0, 0, 0),
-    min_size = Vector2.new(650, 820),
+    min_size = Vector2.new(620, 820),
 })
 
 local PetsTab = window:AddTab("PetsTab")
